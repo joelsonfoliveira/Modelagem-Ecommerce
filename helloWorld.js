e18a52aabc241da8dd4cdb76a6d705c760953dc3
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-11-30 20:41:56 -0300

    Criando classe cliente

--- helloWorld.js
+++
@@ -1,7 +0,0 @@
-var greeting = function (person) {
-  var msg = "Hello world " + person;
-  console.log(msg);
-  return msg;
-};
-var msg = greeting("Abelardo Juvenal");
-//document.body.textContent = greeting(msg);
--- helloWorld.ts
+++
@@ -1,7 +0,0 @@
-const greeting = (person: string) => {
-  const msg = "Hello world " + person;
-  console.log(msg);
-  return msg;
-};
-const msg = greeting("Abelardo Juvenal");
-//document.body.textContent = greeting(msg);
