d9b9081b3866a678564c69adf0eac5410d85d1b0
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-11-30 20:31:48 -0300

    primeiro commit

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+var greeting = function (person) {
+  var msg = "Hello world " + person;
+  console.log(msg);
+  return msg;
+};
+var msg = greeting("Abelardo Juvenal");
+//document.body.textContent = greeting(msg);
---
+++ helloWorld.ts
@@ -0,0 +1,7 @@
+const greeting = (person: string) => {
+  const msg = "Hello world " + person;
+  console.log(msg);
+  return msg;
+};
+const msg = greeting("Abelardo Juvenal");
+//document.body.textContent = greeting(msg);
