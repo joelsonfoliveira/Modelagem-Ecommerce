7b79088465fe34cd5030de74b31d9ada801d337d
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-11-30 21:04:46 -0300

    adicionando propriedades para Cliente

--- Cliente.ts
+++ Cliente.ts
@@ -1,3 +1,13 @@
 class Cliente {
-  constructor(parameters) {}
+  nome: string;
+  CPF: string;
+
+  constructor(nome: string, CPF: string) {
+    this.nome = nome;
+    this.CPF = CPF;
+  }
 }
+
+let cliente = new Cliente("Paulo", "05524230412");
+console.log("Nome: " + cliente.nome);
+console.log("CPF: " + cliente.CPF);
